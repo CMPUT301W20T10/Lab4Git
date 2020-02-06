495b93b1e0ca6591b632718eb8ae48dc61a4ff5b
ALaevens <alaevens@ualberta.ca>
2020-02-05 18:03:04 -0700

    Update shape.java to cause conflict with branches

--- app/src/main/java/com/cmput301w20t10/lab4git/Shape.java
+++ app/src/main/java/com/cmput301w20t10/lab4git/Shape.java
@@ -4,6 +4,8 @@ public abstract class Shape {
 
     protected int x;
     protected int y;
+    
+    String color = "Green";
 
     public int getX() {
         return x;
@@ -20,4 +22,8 @@ public abstract class Shape {
     public void setY(int y) {
         this.y = y;
     }
+    
+    public void setColor(String color) {this.color = color;}
+    
+    public String getColor() {return color;}
 }
